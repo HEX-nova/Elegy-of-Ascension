96965794084d4a33aab1a5c254a47f53b577a27b
HEX-nova <59830185+HEX-nova@users.noreply.github.com>
2026-04-21 08:35:55 +0100

    changes to itemdata, and attempt to customize inventory system

--- Scripts/itemdata.gd
+++ Scripts/itemdata.gd
@@ -1,8 +1,12 @@
 class_name ItemData
 extends Resource
 
+enum Type {consumable, key, tradable, weapon, equipment}
+
 @export var name: String = ""
 @export var weight: float = 1.0
 @export var value: int = 10
 @export var quantity : int = 1
 @export var icon: Texture2D # For UI later
+@export var type : Type = Type.consumable
+@export var description : string = ""
