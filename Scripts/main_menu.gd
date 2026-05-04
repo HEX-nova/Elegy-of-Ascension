816ae2b22ce30c7f1cc7a038e6dd7b67cbd6af0a
HEX-nova <boulhartmilipo@gmail.com>
2026-05-04 13:55:02 +0100

    modded the light , now its global + shaddows. added a basic main menu

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var start = $VBoxContainer/StartButton
+@onready var load = $VBoxContainer/LoadButton
+@onready var exit = $VBoxContainer/ExitButton
+
+@export_file("*.tscn") var first_level_path: String
+
+func _ready() -> void:
+	start.pressed.connect(_on_start_button_pressed)
+	load.pressed.connect(_on_load_button_pressed)
+	exit.pressed.connect(_on_exit_button_pressed)
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file(first_level_path)
+
+func _on_load_button_pressed():
+	print("Searching for save files in user://...")
+
+func _on_exit_button_pressed():
+	get_tree().quit()
